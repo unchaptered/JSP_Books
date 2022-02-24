1fddee54a1a817b23ef7a7b0298cb5ef08b42132
kogyul <dmswo0845@naver.com>
2022-02-24 14:32:48 +0900

    FIX BookDTO.java

--- src/app/book/dao/BookDTO.java
+++ src/app/book/dao/BookDTO.java
@@ -10,103 +10,113 @@ public class BookDTO {
 	private String bookInfo;
 	private String bookInfoShort;
 	private String bookImage;
+	private String bookOrgImage;
 	private String bookPrice;
 	private String bookWriter;
 	private String bookTranslater;
-	private String bookPublished;
+	private String bookPublisher;
+	private String bookCreated;
 	private String bookPages;
 	private String bookMount;
 	private String bookGenre;
 	private String bookCountry;
 	
-	// Getters
-	
 	public String getBookPk() {
 		return bookPk;
+	}
+	public void setBookPk(String bookPk) {
+		this.bookPk = bookPk;
 	}
 	public String getBookTitle() {
 		return bookTitle;
 	}
-	public String getBookSubtitle() {
-		return bookSubtitle;
-	}
-	public String getBookInfo() {
-		return bookInfo;
-	}
-	public String getBookInfoShort() {
-		return bookInfoShort;
-	}
-	public String getBookImage() {
-		return bookImage;
-	}
-	public String getBookPrice() {
-		return bookPrice;
-	}
-	public String getBookWriter() {
-		return bookWriter;
-	}
-	public String getBookTranslater() {
-		return bookTranslater;
-	}
-	public String getBookPublished() {
-		return bookPublished;
-	}
-	public String getBookPages() {
-		return bookPages;
-	}
-	public String getBookMount() {
-		return bookMount;
-	}
-	public String getBookGenre() {
-		return bookGenre;
-	}
-	public String getBookCountry() {
-		return bookCountry;
-	}
-	
-	// Setters
-	
-
-	public void setBookPk(String bookPk) {
-		this.bookPk = bookPk;
-	}
 	public void setBookTitle(String bookTitle) {
 		this.bookTitle = bookTitle;
+	}
+	public String getBookSubtitle() {
+		return bookSubtitle;
 	}
 	public void setBookSubtitle(String bookSubtitle) {
 		this.bookSubtitle = bookSubtitle;
 	}
+	public String getBookInfo() {
+		return bookInfo;
+	}
 	public void setBookInfo(String bookInfo) {
 		this.bookInfo = bookInfo;
+	}
+	public String getBookInfoShort() {
+		return bookInfoShort;
 	}
 	public void setBookInfoShort(String bookInfoShort) {
 		this.bookInfoShort = bookInfoShort;
 	}
+	public String getBookImage() {
+		return bookImage;
+	}
 	public void setBookImage(String bookImage) {
 		this.bookImage = bookImage;
+	}
+	public String getBookOrgImage() {
+		return bookOrgImage;
+	}
+	public void setBookOrgImage(String bookOrgImage) {
+		this.bookOrgImage = bookOrgImage;
+	}
+	public String getBookPrice() {
+		return bookPrice;
 	}
 	public void setBookPrice(String bookPrice) {
 		this.bookPrice = bookPrice;
 	}
+	public String getBookWriter() {
+		return bookWriter;
+	}
 	public void setBookWriter(String bookWriter) {
 		this.bookWriter = bookWriter;
+	}
+	public String getBookTranslater() {
+		return bookTranslater;
 	}
 	public void setBookTranslater(String bookTranslater) {
 		this.bookTranslater = bookTranslater;
 	}
-	public void setBookPublished(String bookPublished) {
-		this.bookPublished = bookPublished;
+	public String getBookPublisher() {
+		return bookPublisher;
+	}
+	public void setBookPublisher(String bookPublisher) {
+		this.bookPublisher = bookPublisher;
+	}
+	public String getBookCreated() {
+		return bookCreated;
+	}
+	public void setBookCreated(String bookCreated) {
+		this.bookCreated = bookCreated;
+	}
+	public String getBookPages() {
+		return bookPages;
 	}
 	public void setBookPages(String bookPages) {
 		this.bookPages = bookPages;
 	}
+	public String getBookMount() {
+		return bookMount;
+	}
 	public void setBookMount(String bookMount) {
 		this.bookMount = bookMount;
+	}
+	public String getBookGenre() {
+		return bookGenre;
 	}
 	public void setBookGenre(String bookGenre) {
 		this.bookGenre = bookGenre;
 	}
+	public String getBookCountry() {
+		return bookCountry;
+	}
 	public void setBookCountry(String bookCountry) {
 		this.bookCountry = bookCountry;
 	}
+	
+	
 }
